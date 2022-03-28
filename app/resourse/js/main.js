884e13cc656a4261cc8642fe9af399455057dcb2
IgorDikal <igrdkl@icloud.com>
2022-03-28 23:03:08 +0300

    "master"

--- app/resourse/js/main.js
+++ app/resourse/js/main.js
@@ -1,23 +1,45 @@
-// $(function() {
-//     $('.add-to-cart').on('click', function(e){
+$(function() {
+    $('.add-to-cart').on('click', function(e){
 
 
-//         e.preventDefault();
-        
-//         let id = $(this).data('id');
+        e.preventDefault();
+        let id = $(this).data('id');
 
-//         console.log(id);
+        console.log(id);
 
-//         $.ajax({
-//             url: 'basket-view',
-//             type: 'POST',
-//             data: {cart: 'add', id: 'id'},
-//             success: function(res){
-//                 console.log(res);
-//             },
-//             error: function(){
-//                 allert('Error');
-//             }
-//         });
-//     });
-// });
+        $.ajax({
+            url: 'basket-cart',
+            type: 'POST',
+            data: {id:'id'},
+
+            success: function(res){
+                console.log(res);
+            },
+            error: function(){
+                allert('Error');
+            }
+        });
+    });
+});
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    // pagination: {
+    //   el: '.swiper-pagination',
+    // },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
